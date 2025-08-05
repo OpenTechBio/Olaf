e3214b6c0217e3471965eddfbd123fb2873e9d2c
Varshini Vijay <varshinivijay05@gmail.com>
2025-08-05 10:19:44 -0700

    initial working prototype revised

--- benchmarking/rag/RetrievalAugmentedGeneration.py
+++ benchmarking/rag/RetrievalAugmentedGeneration.py
@@ -19,7 +19,8 @@ except ImportError as e:
 console = Console()
 SCRIPT_DIR = Path(__file__).resolve().parent
 PARENT_DIR = SCRIPT_DIR.parent
-EMBEDDING_DIR = PARENT_DIR / "rag" / "collections"
+EMBEDDING_DIR = PARENT_DIR / "rag" 
+EMBEDDING_FILE = PARENT_DIR / "rag" / "embeddings.json"
 '''
 1. Function to convert documents into embeddings 
 2. Store the documents in 'collections' folder 
@@ -31,14 +32,31 @@ Compare to the query using cosine similarity
 class RetrievalAugmentedGeneration:
     model = SentenceTransformer('all-MiniLM-L6-v2')
     def __init__(self, query):
-        self.docs = []
+        self.embeddings = load_embeddings()
         self.query = [query]
+
+    def load_embeddings(self):
+        embeddings = []
+        try:
+            with open(EMBEDDING_FILE, "r") as f:
+                for line in f:
+                    embedding = json.loads(line.strip())
+                    embeddings.append(np.array(embedding))
+        except FileNotFoundError as e:
+            console.log(f"[red]Empty or invalid file")
+        return embeddings 
         
+                
     def create_embeddings(self, texts):
-        embeddings = RetrievalAugmentedGeneration.model.encode(texts)
-        with open("", w) as f:
-            f.write(embeddings) 
-        console.print(f"[green]New embeddings successfully generated and stored to: {EMBEDDING_DIR}")
+        embedding = RetrievalAugmentedGeneration.model.encode(texts)
+        try:
+            with open(EMBEDDING_FILE, "w") as f:
+                for emb in embedding:
+                    f.write(json.dumps(emb.tolist()) + "\n")  #convert to list, add newline, and dump as json 
+            self.embeddings.append(embedding)
+            console.print(f"[green]New embeddings successfully generated and stored to {EMBEDDING_FILE}")
+        except ValueError as e:
+            console.print(f"[red]Invalid or missing input")
 
     @classmethod
     def cosine_similarity(cls, A, B):
@@ -50,6 +68,7 @@ class RetrievalAugmentedGeneration:
             
     def query(self, query):
         query_embedding = RetrievalAugmentedGeneration.model.encode([query])
-        sims = cosine_similarity(query_embedding, embeddings)
-        most_similar_idx = np.argmax(sims)
+        sims = cosine_similarity(query_embedding, self.embeddings)
+        idx = np.argmax(sims)
+        return self.embeddings[idx]
     
