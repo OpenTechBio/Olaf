80969dd976716a720ad9512cac154a2aec9d8756
Varshini Vijay <varshinivijay05@gmail.com>
2025-08-05 12:06:27 -0700

    added in functions.json and embeddings.json

--- benchmarking/rag/RetrievalAugmentedGeneration.py
+++ benchmarking/rag/RetrievalAugmentedGeneration.py
@@ -9,6 +9,7 @@ import sys
 try:
     import requests
     import numpy as np
+    import 
     from validators import url as is_url
     from rich.console import Console
 except ImportError as e:
@@ -19,8 +20,9 @@ except ImportError as e:
 console = Console()
 SCRIPT_DIR = Path(__file__).resolve().parent
 PARENT_DIR = SCRIPT_DIR.parent
-EMBEDDING_DIR = PARENT_DIR / "rag" 
-EMBEDDING_FILE = PARENT_DIR / "rag" / "embeddings.json"
+EMBEDDING_DIR = PARENT_DIR / "benchmarking" / "rag" 
+EMBEDDING_FILE = EMBEDDING_DIR / "embeddings.json"
+FUNCTIONS_FILE = EMBEDDING_DIR / "functions.json"
 '''
 1. Function to convert documents into embeddings 
 2. Store the documents in 'collections' folder 
@@ -28,12 +30,21 @@ EMBEDDING_FILE = PARENT_DIR / "rag" / "embeddings.json"
 Encode the query 
 Compare to the query using cosine similarity 
 '''
-
 class RetrievalAugmentedGeneration:
     model = SentenceTransformer('all-MiniLM-L6-v2')
-    def __init__(self, query):
+    def __init__(self):
         self.embeddings = load_embeddings()
-        self.query = [query]
+        self.query = []
+
+    def extract_and_embed_scib(self, url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        func_def = soup.find('h1').get_text(strip=True)
+        func_descr = soup.select_one("dt.sig.sig-object.py").get_text(strip=True)
+        with open(FUNCTIONS_FILE, "w") as f:
+            f.write(json.dumps(func_def.tolist()) + "\n")  
+        create_embeddings(func_descr)
+        return func_def, func_descr
 
     def load_embeddings(self):
         embeddings = []
@@ -67,8 +78,13 @@ class RetrievalAugmentedGeneration:
         return cos_sim        
             
     def query(self, query):
+        self.query.append(query)
         query_embedding = RetrievalAugmentedGeneration.model.encode([query])
         sims = cosine_similarity(query_embedding, self.embeddings)
         idx = np.argmax(sims)
-        return self.embeddings[idx]
-    
+        return idx, self.embeddings[idx]
+
+
+rag = RetrievalAugmentedGeneration()
+rag.extract_and_embed_scib("https://scib-metrics.readthedocs.io/en/latest/generated/scib_metrics.bras.html")
+rag.query("What is scib")
